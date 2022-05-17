27e7af59f2438d10dcdd315df056692cb684e161
molodour <kulisaroslav8@gmail.com>
2022-05-17 16:21:52 +0200

    task number 2

---
+++ NumberExample2.java
@@ -0,0 +1,12 @@
+public class NumberExample2
+{  
+public static void main(String[] args){
+int[] arr = new int[] {11, 22, 33, 44, 55};      
+
+        for(int i = 0; i < 5; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+	}
+}   
