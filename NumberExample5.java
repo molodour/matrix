dda92985b0f76b6b0c0d090c5d2e0382b2c80386
molodour <kulisaroslav8@gmail.com>
2022-05-18 18:35:16 +0200

    Update NumberExample5.java

--- NumberExample5.java
+++ NumberExample5.java
@@ -2,7 +2,7 @@ public class NumberExample5
 {  
 public static void main(String[] args) {
 
-	int[] big = new int[]{32, 5, 23, 1, 3, 3};
+	int[] big = new int[]{-32, 5, -23, 1, 3, 3};
 	
 		for (int i = 1; i < big.length; i++) {
             if(big[i - 1] < 0 && big[i] < 0 || big[i - 1] > 0 && big[i] > 0){
