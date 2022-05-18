675a10812ab7abf187c9a542315e6e86469fd7f5
molodour <kulisaroslav8@gmail.com>
2022-05-18 18:37:10 +0200

    Update NumberExample6.java

--- NumberExample6.java
+++ NumberExample6.java
@@ -2,13 +2,14 @@ public class NumberExample6
 {  
 public static void main(String[] args)   
 {  
-int[] num = new int[]{2, 6, 5, 9, 4};  
-	for (int i =1; i <= num.length; i++){
-		if (num[i-1] < num[i]){
-			if(num[i] > num[i+1]){
-			System.out.println(num[i]);
-				}
+int[] num = new int[] {1, 5, 1, 5, 1};  
+	int pos = 0;
+	for (int i =1; i < 4; i++){
+		if (num[i-1] < num[i] || num[i] > num[i+1]){
+			++pos;		
+			
 			}	
 		}		
+		System.out.println(pos);
 	}
 }
